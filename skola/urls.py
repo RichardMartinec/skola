c923ee7edc8ef3972dd480217c557cd2b9619415
RichardMartinec <114736554+RichardMartinec@users.noreply.github.com>
2024-12-18 12:43:11 +0100

    page

--- skola/urls.py
+++ skola/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('studenti/', views.list_students, name="list-student"),
+    path('ucitelia/', views.list_teachers, name="list-teachers"),
+    path('triedy/', views.list_classes, name="list-classes"),
 ]
